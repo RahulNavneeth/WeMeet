ae227d3ca818342aa2f98602caa13deb919e7a7a
RahulNavneeth <rahulmnavneeth@gmail.com>
2022-02-17 22:56:13 +0530

    Presentation-commit-v3

--- WeMeet/views.py
+++ WeMeet/views.py
@@ -489,6 +489,17 @@ def batchView(request, schoolname, batchurl):
                         z = []
                         stdCount = len(students)
                         msgCount = len(msgs)
+                        batchPostData = []
+
+                        for i in students:
+                            print(i)
+                            
+                            updata = student.objects.get(id=i.id)
+                            udata = User.objects.get(id=updata.user.id)
+                            postData = post.objects.filter(user=udata).all()
+                            for j in postData:
+                                pd = {"id": j.id , 'media': j.media }
+                                batchPostData.append(pd)
 
                         data = [
                             {'BATCH_ID': batchs.id,
@@ -508,9 +519,12 @@ def batchView(request, schoolname, batchurl):
                                         'msgCount': msgCount,
                                         'msgContent': z,
                                     },
-                                }]
+                                }],
+                                'post' : batchPostData
 
                              }]
+
+                        print(data)
                         stdCount = Count(students)
                         for i in students:
                             x = dict(student_id=i.id,
@@ -537,11 +551,12 @@ def batchView(request, schoolname, batchurl):
                                      msgEdit=b.edited
                                      )
                             z.append(a)
-                        s1 = json.dumps(data)
+                        # s1 = json.dumps(data)
 
-                        json_object = json.loads(s1)
 
-                        json_formatted_str = json.dumps(json_object, indent=2)
+                        # json_object = json.loads(s1)
+                        
+                        # json_formatted_str = json.dumps(json_object, indent=2)
 
                         return render(request, 'batch.html', {'data': data})
                         # return HttpResponse(json_formatted_str, content_type="application/json")
